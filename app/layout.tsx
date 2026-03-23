45bfec91f933f4831628808f6d529a761496a250
krishanthan4 <krishanthan2022.4.4@gmail.com>
2026-03-23 14:05:27 +0530

    23-03-2026 [analytics added]

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body className="antialiased font-sans select-none">
+        <Analytics/>
         <Toaster position="bottom-right" richColors />
         {children}
       </body>
